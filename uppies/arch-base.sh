5fcee047e17ecd0cae0d73f7b0de823f998ebd35
sum-catnip <catnip@catnip.fyi>
2022-07-23 20:02:20 +0200

    + uppies

---
+++ uppies/arch-base.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#mkdir -p ~/repos
+#sudo pacman -Syu --noconfirm
+#sudo pacman -S --noconfirm --needed git base-devel reflector stow neovim fish
+
+# installing yay
+#git clone https://aur.archlinux.org/yay.git ~/repos/yay
+#pushd ~/repos/yay
+#cd ~/repos/yay
+#makepkg --noconfirm -si
+#popd
+
+sudo -s <<EOF
+# enable pacman colored output
+sed -i 's/^#Color$/Color/i' /etc/pacman.conf
+# configuring reflector
+cat ./reflector.conf > /etc/xdg/reflector/reflector.conf
+
+# start/enable reflector
+systemctl start reflector
+systemctl enable reflector
+EOF
+
+# install window manager
+yay -aS river --noconfirm
+
+# install visual software
+sudo pacman -S --noconfirm fish firefox alacritty neovide
