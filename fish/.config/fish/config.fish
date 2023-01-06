91174d8232a1ec01afb32eb15abc3b7d2153241b
sum-catnip <catnip@catnip.fyi>
2023-01-06 19:28:56 +0100

    dots update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,9 @@ alias ls exa
 fish_vi_key_bindings
 
 set -px PATH /usr/local/bin /usr/sbin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin
+set -x _JAVA_AWT_WM_NONREPARENTING 1
 set -x MOZ_ENABLE_WAYLAND 1
+set -x QT_QPA_PLATFORM wayland
 set -x FZF_LEGACY_KEYBINDINGS 0
 set -x VISUAL nvim
 set -x WLR_NO_HARDWARE_CURSORS 1
